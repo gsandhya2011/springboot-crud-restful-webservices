4c068a3edc45e11434e69f812c768ebfb697470e
Sandhya Gurrala <srinugsr2020@gmail.com>
2022-01-30 21:50:48 -0600

    initial commit

---
+++ src/test/java/com/sandhya/springboot/SpringbootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandhya.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
